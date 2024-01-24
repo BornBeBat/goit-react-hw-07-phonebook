fce8cda405b24a0686a9cbe23663a30a05414082
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2024-01-24 14:02:21 +0200

    try/catch operations

--- src/myRedux/operations.js
+++ src/myRedux/operations.js
@@ -1,17 +1,38 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import * as api from 'api';
 
-export const fetchContacts = createAsyncThunk('fetchContacts', async () => {
-  const response = await api.getContacts();
-  return response;
-});
+export const fetchContacts = createAsyncThunk(
+  'fetchContacts',
+  async (_, thunkAPI) => {
+    try {
+      const response = await api.getContacts();
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
-export const addContact = createAsyncThunk('addContact', async data => {
-  const response = await api.setContact(data);
-  return response;
-});
+export const addContact = createAsyncThunk(
+  'addContact',
+  async (data, thunkAPI) => {
+    try {
+      const response = await api.setContact(data);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
-export const deleteContact = createAsyncThunk('deleteContact', async id => {
-  const response = await api.deleteContact(id);
-  return response;
-});
+export const deleteContact = createAsyncThunk(
+  'deleteContact',
+  async (id, thunkAPI) => {
+    try {
+      const response = await api.deleteContact(id);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
