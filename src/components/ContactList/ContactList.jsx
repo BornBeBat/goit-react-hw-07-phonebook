39fa65681eb72bb17fbbe5f889f75177d66083cf
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2024-01-24 14:01:17 +0200

    add loader

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,22 @@
 import { Item, List, Text } from './ContactList.styled';
 import { useSelector, useDispatch } from 'react-redux';
-import { filterArray } from 'utils';
-import { getContacts, getFilter } from 'myRedux/selectors';
 import { useEffect } from 'react';
+import { Audio } from 'react-loader-spinner';
+import { filterArray } from 'utils';
+import {
+  getContacts,
+  getFilter,
+  getIsLoading,
+  getError,
+} from 'myRedux/selectors';
 import { deleteContact, fetchContacts } from 'myRedux/operations';
+import { Error } from 'components';
 
 export const ContactList = () => {
   const contacts = useSelector(getContacts);
   const filter = useSelector(getFilter);
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
   const filteredContacts = filterArray(contacts, filter);
   const dispatch = useDispatch();
 
@@ -16,14 +25,32 @@ export const ContactList = () => {
   }, [dispatch]);
 
   return (
-    <List>
-      {filteredContacts.map(({ name, id, phone }) => (
-        <Item key={id}>
-          <Text>Name: {name}</Text>
-          <Text>Phone: {phone}</Text>
-          <button onClick={() => dispatch(deleteContact(id))}>Delete</button>
-        </Item>
-      ))}
-    </List>
+    <>
+      {error && <Error />}
+      {isLoading && (
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="loading"
+          wrapperStyle
+          wrapperClass
+        />
+      )}
+      {!isLoading && (
+        <List>
+          {filteredContacts.map(({ name, id, phone }) => (
+            <Item key={id}>
+              <Text>Name: {name}</Text>
+              <Text>Phone: {phone}</Text>
+              <button onClick={() => dispatch(deleteContact(id))}>
+                Delete
+              </button>
+            </Item>
+          ))}
+        </List>
+      )}
+    </>
   );
 };
